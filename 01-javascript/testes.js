f5e9e52e574f302e0fffec826805a95ae6fbab59
alissondesenvolvedordemidias-dev <alissondesenvolvedordemidias@gmail.com>
2026-04-16 22:11:57 -0300

    index.html

---
+++ 01-javascript/testes.js
@@ -0,0 +1,22 @@
+// alert ("Salve!")
+// alert ("Fala comigo bb")
+
+// let nome 
+
+// nome = "Alisson"
+
+// alert ("Nome: " + nome)
+
+// let idade  = 26 
+// alert("Idade: " + idade)
+
+// alert("Nome: " + nome + "\nidade: " + idade)
+
+// nome = prompt("Digite seu nome:")
+// alert("Nome digitado: " + nome)
+
+let nota1
+nota1 = prompt("Digite a primeira nota: ") 
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
